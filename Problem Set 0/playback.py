d477f151731a0fc05296e6a6f38ab99c9d1d62fd
Wahaj18 <48466561+Wahaj18@users.noreply.github.com>
2024-01-08 16:45:06 +0500

    Add files via upload

---
+++ Problem Set 0/playback.py
@@ -0,0 +1,15 @@
+
+print(input().replace(" ","..."))
+
+#print("...".join(input().split()))
+
+# def insertplayback(inputstr="as as"):
+#     inputstrarray = inputstr.split()
+#     playback = "..."
+#     playback = playback.join(inputstrarray)
+#     print(playback)
+
+# def main():
+#     insertplayback(input())
+
+# main()
